7e7d6e3dc9e7d4406b0788776b6ae2fd482f7de5
Sampath258 <chilukurisam3@gmail.com>
2025-02-08 02:31:16 -0500

    Initial commit

---
+++ TriangleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿                //NAME:CHILUKURI SAMPATH(Mutant Version)
+
+namespace TriangleApp{
+public class Triangle
+{
+    public static bool IsIsosceles(int a, int b, int c)
+    {
+        //checking equilateral Triangle instead of isosceles Triangle to chect mutation
+        return a == b && b == c  ;
+    }
+    public static void Main(string[] args){
+        Console.WriteLine("TestExecuted by Chilukuri Sampath");
+        Console.WriteLine(IsIsosceles(5,4,5));
+    }
+}
+}
+
---
+++ TriangleTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿        
+        // NAME : CHILUKURI SAMPATH
+using NUnit.Framework;
+using TriangleApp;
+namespace TriangleTests{
+    [TestFixture]
+public class TriangleTests
+{
+    [Test]
+    public void TestIsosceles()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Sampath");
+        //It fails while checking equilateral triangle.So 2 tests will succeed and one will fail
+         Assert.That(Triangle.IsIsosceles(4, 5, 4),Is.True);  
+
+    }
+     [Test]
+    public void Test_NoteIsosceles()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Sampath");
+        Assert.That(Triangle.IsIsosceles(4, 5,6),Is.False);  
+    }
+     [Test]
+    public void TestEquilateral()
+    {
+        Console.WriteLine("TestExecuted by Chilukuri Sampath");
+        Assert.That(Triangle.IsIsosceles(4, 4, 4),Is.True);  
+    }
+}
+}
